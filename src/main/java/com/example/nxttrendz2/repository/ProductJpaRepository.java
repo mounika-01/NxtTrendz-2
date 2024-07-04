7cff47ffde39a3614b5ab47df836bbce04855b10
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-07-04 16:34:12 +0530

    Update ProductJpaRepository.java

--- src/main/java/com/example/nxttrendz2/repository/ProductJpaRepository.java
+++ src/main/java/com/example/nxttrendz2/repository/ProductJpaRepository.java
@@ -2,7 +2,7 @@ package com.example.nxttrendz2.repository;
 
 import com.example.nxttrendz2.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springfrmaework.sterotype.Repository;
+import org.springframework.stereotype.Repository;  
 
 @Repository
 public interface ProductJpaRepository extends JpaRepository<Product, Integer> {
