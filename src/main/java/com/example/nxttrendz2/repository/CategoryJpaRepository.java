8f27e3bdad36a2c55f36e3dd8586d15546deb2f8
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-07-09 17:39:10 +0530

    Update CategoryJpaRepository.java

--- src/main/java/com/example/nxttrendz2/repository/CategoryJpaRepository.java
+++ src/main/java/com/example/nxttrendz2/repository/CategoryJpaRepository.java
@@ -1,8 +1,10 @@
 package com.example.nxttrendz2.repository;
+
 import com.example.nxttrendz2.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;  
+import org.springframework.stereotype.Repository;
+
 @Repository
-public interface CategoryJpaRepository extends JpaRepository<Category, Integer> { 
-    
+public interface CategoryJpaRepository extends JpaRepository<Category, Integer> {
+
 }
